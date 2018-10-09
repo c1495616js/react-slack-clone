8cab9b1397d82e2a0b4c0fa220a18c7b3cf123e4
jerrywang761208 <c1495616@gmail.com>
2018-10-09 21:09:30 +0800

    register form init

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+
+var config = {
+  apiKey: "AIzaSyBzL_RZg1sAdEk_50W8VOvSq06lY6cOcpg",
+  authDomain: "react-slack-clone-795dc.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-795dc.firebaseio.com",
+  projectId: "react-slack-clone-795dc",
+  storageBucket: "react-slack-clone-795dc.appspot.com",
+  messagingSenderId: "311668320898"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
