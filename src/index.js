c712994f90c44b873e0f8b89bc4a0bf4185bb2a6
jerrywang761208 <c1495616@gmail.com>
2018-10-14 01:32:33 +0800

    set up redux to make user data acailable on globale state

--- src/index.js
+++ src/index.js
@@ -5,39 +5,74 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from 'react-router-dom'
 import firebase from './firebase';
 import 'semantic-ui-css/semantic.min.css';
 
+import {
+  createStore
+} from 'redux';
+import {
+  Provider,
+  connect
+} from 'react-redux';
+import {
+  composeWithDevTools
+} from 'redux-devtools-extension';
+
+import rootReducers from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducers, composeWithDevTools());
 class Root extends React.Component {
   componentDidMount = () => {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     })
   }
-  
 
-  render(){
+
+  render() {
     return (
 
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
+      <Switch >
+      <Route exact path = "/"
+      component = {
+        App
+      }
+      /> 
+      <Route path = "/login"
+      component = {
+        Login
+      }
+      /> 
+      <Route path = "/register"
+      component = {
+        Register
+      }
+      /> 
+      </Switch>
 
     )
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
-ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>
+ReactDOM.render( 
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import * as actionTypes from '../actions/types'
+import { combineReducers } from 'redux';
+
+const initialUser = {
+  currentUser: null,
+  isLoading: false
+}
+
+const user_reducer = (state = initialUser, action) => {
+  switch(action.type){
+    case actionTypes.SET_USER:
+      return {
+        currentUser: action.payload.currentUser,
+        loading: false
+      }
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  user: user_reducer
+})
+
+export default rootReducer;
