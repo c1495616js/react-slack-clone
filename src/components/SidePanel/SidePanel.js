9a623834dee77eb5251c4353ec98672aa7252054
jerrywang761208 <c1495616@gmail.com>
2018-10-25 11:06:46 -0700

    Add Channels Componet

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
-import UserPanel from './UserPanel'
-
+import UserPanel from './UserPanel';
+import Channels from './Channels';
 export default class SidePanel extends Component {
   render() {
     const {currentUser} = this.props;
@@ -16,6 +16,7 @@ export default class SidePanel extends Component {
       >
         
         <UserPanel currentUser={currentUser}/>
+        <Channels />
       </Menu>
     )
   }
