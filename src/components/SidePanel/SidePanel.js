b62bfec9447a47f3588dfc07ca6708f9a128b297
jerrywang761208 <c1495616@gmail.com>
2018-10-26 18:51:49 -0700

    create channel collections. Add first channel to database

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -15,8 +15,8 @@ export default class SidePanel extends Component {
         style={{background: '#4c3c4c', fontSize: '1.2rem'}}
       >
         
-        <UserPanel currentUser={currentUser}/>
-        <Channels />
+        <UserPanel currentUser={currentUser} />
+        <Channels currentUser={currentUser} />
       </Menu>
     )
   }
