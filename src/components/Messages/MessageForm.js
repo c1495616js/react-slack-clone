d8218a92a0ae807020c5165bfe0effa505e38c03
jerrywang761208 <c1495616@gmail.com>
2018-10-30 09:18:13 -0700

    create File Modal to upload File

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import firebase from '../../firebase';
+import FileModal from './FileModal'
 import { Segment, Button, Input } from 'semantic-ui-react'
 export default class MessageForm extends Component {
   state = {
@@ -7,8 +8,13 @@ export default class MessageForm extends Component {
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     loading: false,
-    errors: [] 
+    errors: [],
+    modal: false
   }
+
+  openModal = () => this.setState({modal: true});
+  closeModal = () => this.setState({modal: false});
+
 
   handleChange = event => {
     this.setState({[event.target.name]: event.target.value})
@@ -56,7 +62,7 @@ export default class MessageForm extends Component {
   }
 
   render() {
-    const { errors, message, loading } = this.state;
+    const { errors, message, loading, modal } = this.state;
     return (
       <Segment className="message__form">
         <Input 
@@ -85,9 +91,14 @@ export default class MessageForm extends Component {
           />
           <Button 
             color="teal"
+            onClick={this.openModal}
             content="Upload Media"
             labelPosition="right"
             icon="cloud upload"
+          />
+          <FileModal 
+            modal={modal}
+            closeModal={this.closeModal}
           />
         </Button.Group>
       </Segment>
